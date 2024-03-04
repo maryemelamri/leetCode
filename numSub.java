5483585d4cda9d1daee84f6a78dfee27d03ac86b
maryem elamri <61750815+maryemelamri@users.noreply.github.com>
2024-03-04 12:24:58 +0100

    Update Number of Substrings With Only 1s

--- numSub.java
+++ numSub.java
@@ -1,20 +1,25 @@
 class Solution {
     public int numSub(String s) {
-         int count = 0;
-        int result = 0;
-        for(char c : s.toCharArray()){
+        long count = 0;
+        long result = 0;
+       long l =1000000007L;
+
+        for(int i = 0; i < s.length(); i++){
+            Character c = s.charAt(i);
             if( c== '1')
                 count++;
-            else{
+            else{   
                 result+= ((count+1)* count)/2;
                 count=0;
             }
 
-
         }
-            result+= ((count+1)* count)/2;
-            return result;
-    
+        result += (   count * (count + 1)) / 2;
+        //    return result;
+        System.out.println( result +"result");
+        int   res= (int) (result % (Math.pow(10,9)+7));
+        System.out.println( res +"res");
+          return res;
     
     }
 }
